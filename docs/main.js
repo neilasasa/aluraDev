23ca3cf04cdecd13e63b8320819c6c2b53bed72b
Neila Dutra <66982837+neilasasa@users.noreply.github.com>
2021-12-20 10:43:20 -0500

    included community.html and changed some css

---
+++ docs/main.js
@@ -0,0 +1,37 @@
+/* 
+    CHECAR DATA ATTRIBUTE !!! 
+    CHECAR MODULE & COMPONENTS 
+*/
+
+//evitando o codigo estar no escopo global (esconder do browser) podemos utilizar uma funcao anonima com parenteses ao final
+// (() => { ...todo codigo aqui })()
+
+(() => {
+    // CODE TO USE HIGHLIGHT BUTTON
+    const linguagem = document.querySelector(`.linguagem-item`);
+    const areaCodigo = document.querySelector(`.codigo-wrapper`);
+    const botao = document.querySelector(`.botao`);
+
+    function aplicaHighlight() {
+        const codigo = areaCodigo.innerText;
+        areaCodigo.innerHTML = `<code class="preview hljs ${linguagem.value}" contenteditable="true" area-label="Editor de codigo"></code>`;
+        areaCodigo.querySelector(`code`).textContent = codigo;
+        hljs.highlightElement(areaCodigo.querySelector(`code`));
+    }
+
+    botao.addEventListener(`click`, () => {
+        aplicaHighlight()
+    });
+
+    // CODE TO CHANGE COLOR
+    const colorBox = document.querySelector(`.projeto__code--boxOut`);
+    const color = document.querySelector(`.projeto__new--personalizacao-cor`);
+
+    setColor()
+    color.addEventListener(`input`, setColor);
+
+    function setColor() {
+        colorBox.style.background = color.value
+    }
+
+})()
